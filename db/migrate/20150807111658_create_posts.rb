7e8757906f1a514b043a94c178a130da062cf7f6
Lowell Mower <lowell.m.mower@gmail.com>
2015-08-07 09:11:54 -0400

    generate contorllers for admin and user

---
+++ db/migrate/20150807111658_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.string :url, null: false
+      t.string :description, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
