b181fbc712338239377931c48d036ce7f49c4e75
Lowell Mower <lowell.m.mower@gmail.com>
2015-08-30 13:38:22 -0400

    change user on deploy to root

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '45.55.63.133', user: 'deploy', roles: %w{web app}
+server '45.55.63.133', user: 'root', roles: %w{web app}
 
 
 # server-based syntax
