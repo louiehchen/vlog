4533278b858bab2c5e628071b16390307905af81
Lowell Mower <lowell.m.mower@gmail.com>
2015-08-30 11:28:15 -0400

    final capistrano changes for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,9 @@
+set :stage, :production
+
+# Replace 127.0.0.1 with your server's IP address!
+server '45.55.63.133', user: 'deploy', roles: %w{web app}
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
