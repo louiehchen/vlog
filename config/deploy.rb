e7cb36037c65b0cfd9e50f485de3d6e17e4ce480
Lowell Mower <lowell.m.mower@gmail.com>
2015-08-31 13:48:39 -0400

    change linked files to root

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :ssh_options, {
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml .rbenv-vars .ruby-version}
+set :linked_files, %w{config/database.yml config/secrets.yml ~/.rbenv-vars .ruby-version}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
