ccb685242937574b3b40f8e0d9203adf06c42832
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-02 18:55:12 -0400

    add route for app index and first app

--- config/routes.rb
+++ config/routes.rb
@@ -13,59 +13,13 @@ Rails.application.routes.draw do
   resources :posts
 
   get '/posts_json' => 'posts#all_posts'
-
   get '/projects' => 'projects#index'
-
   get '/about' => 'welcome#about'
-
   get '/apps' => 'apps#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  namespace :apps do
+    resources :background, :only => [:index]
+    resources :talking, :only => [:index]
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
