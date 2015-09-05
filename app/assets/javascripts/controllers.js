e0234aa0b7ac1d731db85a9f255cb3c8689c1b75
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-05 11:47:53 -0400

    remove console debugging

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -3,7 +3,7 @@ var vlogControllers = angular.module('vlogControllers', []);
 vlogControllers.controller('getPosts', function($scope, $http){
   $http.get('/posts_json').success(function(data){
     $scope.posts = data;
-    console.log(data)
+    // console.log(data)
   });
   // eventually order by created at
   $scope.orderProp = 'title';
