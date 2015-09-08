44cdfbcf773470600609a5396f937980feac4558
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-08 10:04:23 -0400

    add route for todo app

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   namespace :apps do
     resources :background, :only => [:index]
+    resources :todo, :only => [:index]
   end
 
   # Example of regular route:
