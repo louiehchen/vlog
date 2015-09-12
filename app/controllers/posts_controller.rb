134185aa6dfd76fcdfe1d94b350ec6ed99210a62
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-12 18:34:42 -0400

    permit category into params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,6 +53,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:url, :title, :subtitle, :description)
+    params.require(:post).permit(:url, :title, :subtitle, :description, :category)
   end
 end
