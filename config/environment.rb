8dfdedbab61d828b2e89a9f126c19186f70e0ed0
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-14 18:35:32 -0400

    change domain URL

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['MAILCHIMP_USERNAME'],
   :password => ENV['MAILCHIMP_API'],
-  :domain => 'http://localhost:3000',
+  :domain => 'http://www.lowellmower.com',
   :address => 'smtp.mandrillapp.com',
   :port => 587,
   :authentication => :plain,
