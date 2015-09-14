83ce966628c0d14229b0f3dd45fb79062a3ee86e
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-14 16:54:17 -0400

    remove undefined variable

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "#{mail_info[:name]}"
+  default from: "lowell@example.com"
   def admin_contact(mail_info)
     mail(to: "lowell.mower@gmail.com", subject: "Contact Message", body: mail_info[:body], content_type: "text/html")
   end
