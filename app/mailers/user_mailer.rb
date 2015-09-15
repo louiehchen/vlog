ab30a68b10e5a41c59b1c04031417b67f8a3bf98
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-14 23:23:15 -0400

    change subject line and style code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ApplicationMailer
   default from: "lowell@example.com"
+
   def admin_contact(mail_info)
-    mail(to: "lowell.mower@gmail.com", subject: "Contact Message", body: mail_info[:body], content_type: "text/html")
+    mail(
+      to: "lowell.mower@gmail.com",
+      subject: mail_info[:name] + " || " + mail_info[:email],
+      body: mail_info[:body],
+      content_type: "text/html")
   end
+
 end
