1d387b5b1bfc8e11bddba5f8ae2b0ea9c7cbb41f
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-14 22:16:34 -0400

    remove unnecessary method

--- app/controllers/mailer_controller.rb
+++ app/controllers/mailer_controller.rb
@@ -19,12 +19,3 @@ class MailerController < ApplicationController
   end
 
 end
-
-
-  def email_followers(deal)
-    @vendor = Vendor.find(deal.vendor_id)
-    user_emails = @vendor.followers.pluck(:email)
-    @deal = deal
-    @url  = 'https://afternoon-meadow-3523.herokuapp.com/'
-    mail(to: user_emails, subject: "New Deals from #{@vendor.name}")
-  end
