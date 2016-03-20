f2b5f4ba078bdc55e8320e5e74b3aab0be71c077
lowellmower <lowell.mower@gmail.com>
2016-03-20 08:49:22 -0400

    controller actions for getting weather

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,16 @@ class ApplicationController < ActionController::Base
 
   def load_pseudo_user
   	@pseudo_user = PseudoUser.new
+  	gather_lat_lon(@pseudo_user)
+  end
+
+  def gather_lat_lon(sudo_user)
+  	begin 
+  		loc = request.location
+  		sudo_user.assign_attributes(lat: loc.latitude, lon: loc.longitude)
+  	rescue => e
+  		Rails.logger.error('Issue with geocoder: #{e.message}')
+  	end
   end
   
 end
--- app/controllers/apps/weather_controller.rb
+++ app/controllers/apps/weather_controller.rb
@@ -1,9 +1,13 @@
 class Apps::WeatherController < ApplicationController
 
-	before_action :load_pseudo_user, only: [:index]
+	before_action :load_pseudo_user, only: [:index, :get_weather_data]
 
 	def index
-		
+	end
+
+	def get_weather_data
+		test_data = WeatherHelper.get_weather_by_ip(@pseudo_user)
+		render text: test_data
 	end
 
 end
